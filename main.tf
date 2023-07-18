5d0a4db65529afc207c5841e3a67a3754ad29b81
kakarlavenu <137424408+nubiiojm@users.noreply.github.com>
2023-07-18 22:20:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "venujenkins11"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "venu11"
-  }
-}
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
