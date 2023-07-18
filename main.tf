5def07fe4c4396f71d7499b267f20fac25ba9904
kakarlavenu <137424408+nubiiojm@users.noreply.github.com>
2023-07-18 22:01:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "<your_bucket_name>"
+    key            = "terraform.tfstate"
+    region         = "<your_aws_region>"
+    dynamodb_table = "<your_dynamo_dbtable_name>"
+  }
+}
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
